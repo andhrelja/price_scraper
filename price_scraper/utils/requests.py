ac7d459eb0f0b95ee0db1e659f1f1bb97c9ddc3b
Andrea Hrelja <34381530+andhrelja@users.noreply.github.com>
2024-08-07 08:00:45 +0200

    Update requests.py

--- price_scraper/utils/requests.py
+++ price_scraper/utils/requests.py
@@ -7,7 +7,11 @@ logger = logging.getLogger(__name__)
 
 def get(url, *args, **kwargs):
     # client = httpx.Client(http2=True)
-    response = requests.get(url, *args, **kwargs)
+    try:
+        response = requests.get(url, *args, **kwargs)
+    except requests.exceptions.ConnectTimeout as e:
+        logger.warning("Connection timeout occured for %s", url)
+        return
     logger.debug("args: %s", json.dumps(args, indent=2))
     logger.debug("kwargs: %s", json.dumps(kwargs, indent=2))
     logger.debug("response: %s", json.dumps(response, default=str, indent=2))
