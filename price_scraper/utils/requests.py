71e890376672a8acdd5846240ce890ffb0c3b8c9
andhrelja <andhrelja@hotmail.com>
2024-10-07 16:43:34 +0200

    fix: status code handling

--- price_scraper/utils/requests.py
+++ price_scraper/utils/requests.py
@@ -9,15 +9,22 @@ def get(url, *args, **kwargs):
     # client = httpx.Client(http2=True)
     try:
         response = requests.get(url, *args, **kwargs)
-    except requests.exceptions.ConnectTimeout as e:
+    except requests.exceptions.ConnectTimeout:
         logger.warning("Connection timeout occured for %s", url)
         return
     logger.debug("args: %s", json.dumps(args, indent=2))
     logger.debug("kwargs: %s", json.dumps(kwargs, indent=2))
     logger.debug("response: %s", json.dumps(response, default=str, indent=2))
-    if response.status_code != 404:
-        response.raise_for_status()
+
     if response.status_code == 404:
         logger.warning("Page not found: %s", url)
         return
+    elif response.status_code == 403:
+        logger.warning("Forbidden for URL: %s", url)
+        return
+    elif response.status_code >= 500:
+        logger.warning("Server error for URL: %s", url)
+        return
+    else:
+        response.raise_for_status()
     return response
