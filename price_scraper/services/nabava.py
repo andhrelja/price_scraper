c79914022b2b3da910e4a2279dac5a77ef35d412
andhrelja <andhrelja@hotmail.com>
2025-04-15 15:23:44 +0200

    fix(nabava): parse HTML instead of JSON

--- price_scraper/services/nabava.py
+++ price_scraper/services/nabava.py
@@ -4,7 +4,6 @@
 
 from typing import Iterable
 from bs4 import BeautifulSoup
-import json
 
 from price_scraper import models
 from price_scraper import repository
@@ -14,14 +13,18 @@ SCRIPT_INDEX = 0
 
 def html_parser(html_text: str, features="html.parser") -> Iterable:
     soup = BeautifulSoup(html_text, features)
-    scripts = soup.find_all("script", type="application/ld+json")
-    return json.loads(scripts[SCRIPT_INDEX].string)
+    offer = soup.find("div", id="offer-with-min-price")
+    price = offer.find_next("div", class_="offer__price")
+    price = price.text.replace("€", "").strip()
+    price = price.replace(".", "")
+    price = price.replace(",", ".")
+    return price
 
 
 def apply(html_text: str, **kwargs) -> models.Product:
     parsed = html_parser(html_text)
     if not parsed:
         return parsed
-    product = models.Product(price=parsed["offers"]["lowPrice"], **kwargs)
+    product = models.Product(price=parsed, **kwargs)
     repository.Product.add(product.asdict())
     return product
